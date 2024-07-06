6c4b287ab96bbc15b39398c471178e82fcd4edf1
ellisroberta <ellisroberta@hotmail.com>
2024-07-06 10:30:19 -0300

    commit inicial

---
+++ src/test/java/com/example/taxcalculator/TaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
